717b969fb8ee259a2f0e7e2a54f0b0c56da35f5e
Pedro Fernely Uch Puc <uchpuc@gmail.com>
2026-09-23 11:09:36 -0600

    Add mathematical operations module with placeholders
    Created a module for basic mathematical operations with placeholder functions for addition, subtraction, multiplication, and division.

---
+++ operaciones.py
@@ -0,0 +1,20 @@
+"""
+MÓDULO DE OPERACIONES MATEMÁTICAS
+Cada integrante del equipo tiene asignada una función específica.
+"""
+
+def sumar(a, b):
+    # TODO: Estudiante 2
+    pass
+
+def restar(a, b):
+    # TODO: Estudiante 3
+    pass
+
+def multiplicar(a, b):
+    # TODO: Estudiante 4
+    pass
+
+def dividir(a, b):
+    # TODO: Estudiante 5
+    pass
